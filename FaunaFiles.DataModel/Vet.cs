3da6a70f2d015e798488ccd12d0276803b66ce8a
Karol Legaspi <Karol Legaspi@MSI>
2025-08-05 10:23:14 +0800

    Add Hello // try

--- FaunaFiles.DataModel/Vet.cs
+++ FaunaFiles.DataModel/Vet.cs
@@ -15,6 +15,7 @@ namespace FaunaFiles.DataModel
         public string PhoneNumber { get; set; } 
         public string Address { get; set; }
         public string ImagePath { get; set; }
+        //Hello
         
     }
 }
